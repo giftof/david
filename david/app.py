1081dc3b86f13320706145cc68635f1dbd90a562
cloudeven <cloudeven@gmail.com>
2025-07-15 22:04:12 +0900

    setup

--- david/app.py
+++ david/app.py
@@ -1,21 +1,66 @@
-from flask import Flask, request, Response
+import base64
+from datetime import datetime
+from flask import Flask, request, Response, jsonify, render_template
 import os
 from io import BytesIO
 from gtts import gTTS
+from enum import Enum
 
 DEFAULT_LANG = os.getenv('DEFAULT_LANG', 'ko')
 app = Flask(__name__)
 
-@app.route("/")
+class LANGS(str, Enum):
+    KO = 'ko'
+    EN = 'en'
+    JA = 'ja'
+    ES = 'es'
+
+LOG_FILE = 'input_log.txt'
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
+    if request.method == 'GET':
+        return render_template("index.html")
+    
+    if request.method == 'POST':
+        input_text = request.form.get('input_text')
+        lang = request.form.get('lang', 'ko')
 
-    text = "Hello, DevOps"
+        if lang not in [l.value for l in LANGS]:
+            return render_template("index.html", error=f"언어 설정 오류 received: [{lang}]", selected_lang='ko')
+        
+        if not input_text:
+            return render_template("index.html", error="입력된 문장이 없습니다.", selected_lang=lang)
+        else:
+            with open(LOG_FILE, 'a', encoding='utf-8') as f:
+                log_entry = f"{datetime.now().isoformat()} | lang={lang} | input_text={input_text}\n"
+                f.write(log_entry)
 
-    lang = request.args.get('lang', DEFAULT_LANG)
-    fp = BytesIO()
-    gTTS(text, "com", lang).write_to_fp(fp)
+        try:
+            fp = BytesIO()
+            gTTS(input_text, "com", lang).write_to_fp(fp)
+            audio_data = base64.b64encode(fp.getvalue()).decode("utf-8")
+        except ValueError as e:
+            return render_template("index.html", error=e, selected_lang=lang)
 
-    return Response(fp.getvalue(), mimetype='audio/mpeg') # 페이지 전달없이 바로 재생
+        return render_template("index.html", audio=audio_data, selected_lang=lang)
+
+    return render_template("index.html", selected_lang='ko')
+
+
+
+        # return Response(fp.getvalue(), mimetype='audio/mpeg') # 페이지 전달없이 바로 재생
+
+
+# @app.route("/sample", methods=['POST'])
+# def default_post():
+#     data = request.get_json()  # JSON 데이터 받아오기
+#     lang = data.get('lang')
+#     input_text = data.get('input_text')
+#     return jsonify({
+#         'message': f'Hello, {lang}. {input_text}.'
+#     }), 200
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
